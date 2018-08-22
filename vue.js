06075ccd421be000a51345306e030215978f1a58
albsosa <inf.albsosa@gmail.com>
2018-08-21 19:46:00 -0500

    Lista de actividades al 100

--- vue.js
+++ vue.js
@@ -1,16 +1,39 @@
 var app = new Vue({
-	el : '#myapp',
+	el : '#app',
 	data: {
-		primero :0,
-		segundo :0,
-		tercero :0,
-		cuarto :0,
-		suma : 0
+		titulo : 'Lista de Tareas', 
+		tareas : [
+			{
+				texto: 'Aprender Vue.js',
+				terminada: false
+			},
+			{
+				texto: 'Ponerme a trabajar',
+				terminada: false
+			},
+			{
+				texto: 'Irme a dormir ',
+				terminada: false
+			}
+		], 
+		//Se crea una nueva propiedad
+		nuevaTarea: ''
+
 	},
-	//Esta parte se va haciendo el calculo al momento computed:
-	computed: {
-		total: function(){
-			return this.primero + this.segundo + this.tercero + this.cuarto;
+	methods: {
+		agregarTarea: function(){
+			var texto = this.nuevaTarea.trim();
+			if(texto){
+				//para poder agregar la tarea al  arreglo se necesita el push 
+				this.tareas.push({
+					texto: texto,
+					terminada: false
+				})
+			}
+			this.nuevaTarea= ''
+		},
+		borrar: function(indice){
+			this.tareas.splice(indice,1)
 		}
 	}
 })
